d60766841890527da2e06d2e698fa70f6dc6dd59
Mashal Hamid <mashallight99@gmail.com>
2022-04-12 02:47:53 +0800

    create input widged

---
+++ lib/input.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+//default way to wrtie a statefulwidget
+class Input extends StatefulWidget {
+  const Input({Key? key}) : super(key: key);
+
+  @override
+  _Input createState() => _Input(); //subclass
+}
+
+class _Input extends State<Input> {
+  final int number = 0;
+  // ignore: use_key_in_widget_constructors
+  _Input();
+
+
+//First text
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(30),
+      child: TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter a number',
+            ),
+          ),
+    );
+  }
+}

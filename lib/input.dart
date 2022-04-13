549a7c7af803c3b1bf63293e45b4009de7d21d5b
Mashal Hamid <mashallight99@gmail.com>
2022-04-13 20:32:41 +0800

    added the enter button, linked the input box to the list.

--- lib/input.dart
+++ lib/input.dart
@@ -1,37 +1,92 @@
-// ignore_for_file: file_names
+// ignore: file_names
+// ignore_for_file: file_names, deprecated_member_use, use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-//default way to wrtie a statefulwidget
 class Input extends StatefulWidget {
-  const Input({Key? key}) : super(key: key);
+  final wordle; 
+  const Input(this.wordle);
 
   @override
-  _Input createState() => _Input(); //subclass
+  _InputState createState() => _InputState(wordle);
 }
 
-class _Input extends State<Input> {
-  final number = TextEditingController();
-  // ignore: use_key_in_widget_constructors
-  _Input();
 
+class _InputState extends State<Input> {
 
-//First text
+  
+  final numController = TextEditingController();
+  final wordle; 
+
+  @override
+  void dispose() {
+    numController.dispose();
+    super.dispose();
+  }
+
+  _InputState(this.wordle);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.all(30),
-      child:Column(
-          children: [
-            TextField(
-              controller: number,
-              decoration: const InputDecoration(
-                hintText: 'Enter a number',
-              ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children:  <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+          child: TextField(
+            controller: numController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a number',
+            ),
+          ),
+        ),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 150, vertical: 25),
+          child: RaisedButton(
+            onPressed: () => { 
+            if((int.parse(numController.text)) > wordle.length || (int.parse(numController.text)) <  0 ){
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text("There are only ${wordle.length} Days Answers"),
+                    );
+                    },
             )
-          ],
-      )
+            }
+            else{
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text(
+                      wordle[(int.parse(numController.text))-1],
+                      textAlign: TextAlign.center),
+                    );
+                  },
+              )
+            }
+            
+          },
+            child:ConstrainedBox(
+              constraints: BoxConstraints(minWidth: 300),
+              child:Container(
+              child: Text(
+                      "ENTER", 
+                      style: TextStyle(
+                            color: Colors.white, 
+                            fontSize: 25
+                          )
+                    ),
+              
+              width:300,
+              height: 50,
+              alignment: Alignment.center
+              )),
+              color: Color.fromARGB(255, 11, 133, 62), //Enter Button
+          ),)],
     );
   }
 }
